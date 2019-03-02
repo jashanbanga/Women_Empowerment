36b5725f16f8400bf8a23fe244e0f1fc13430f6b
AniketShahane <shahaneaniket11@gmail.com>
2019-03-02 14:33:08 +0530

    Add organisation model

--- womenEmpowerment/womenEmpowerment/settings.py
+++ womenEmpowerment/womenEmpowerment/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'organization'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'womenEmpowerment.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +119,24 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'womenEmpowerment/static')
+]
+
+#Media Files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+#Messages
+from django.contrib.messages import constants as messages 
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+#Email setup 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'see.you.travels.00@gmail.com'
+EMAIL_HOST_PASSWORD = 'seeyou@321'
+EMAIL_USE_TLS = True
